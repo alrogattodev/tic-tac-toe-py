3e1431a9f96b12a2361908ce963bcd56f5fdad1a
Al Rogatto <alrogattodev@gmail.com>
2024-10-10 23:00:12 -0300

    Update tic-tac-toe.py
    draw_move function added.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,3 +1,5 @@
+from random import randrange
+
 board_status = [1, 2, 3], [4,'X',6], [7,8,9]
 occupied_fields = []
 def dislpay_board(board_status):
@@ -9,8 +11,8 @@ def dislpay_board(board_status):
         print(empty_row)
         value_row = ""        
         for n in board_status[i]:
-            value_row += "|---" + str(n) + "---| "
-        print(value_row, end="\n")
+            value_row += "|   " + str(n) + "   | "
+        print('\x1b[6;30;42m' + value_row + '\x1b[0m', end="\n", )
         print(empty_row)        
         print(header_row)
     print("\n-----------------------------\n")
@@ -26,17 +28,34 @@ def enter_move(board_status):
             else: 
                 board_status[line][col] = "O"
                 dislpay_board(board_status)
+                draw_move(board_status)
                 break
         except: 
             print("Mysterious error!")
             enter_move(board_status)
 
 def make_list_of_free_fields(board_status):
-    for line in range(len(board_status)):
-        for col in range(len(board_status[line])):
-            if board_status[line][col] == 'X' or board_status[line][col] =='O' :
-                occupied_fields.append((line, col))
-    return occupied_fields
+    return 0
+    
+def draw_move(board_status):
+    while True:
+        try: 
+            for i in range(10):
+                pos = randrange(8)
+
+            line = (pos -1) // 3
+            col = (pos -1) % 3
+            if board_status[line][col] == 'X' or board_status[line][col] == 'O':
+                print("Position already occupied, please chose another.")
+            else: 
+                board_status[line][col] = "X"
+                dislpay_board(board_status)
+                break
+        except: 
+            print("Mysterious error!")
+            draw_move(board_status)
+
+def victory_for(board_status sign):
     
 
 dislpay_board(board_status)
